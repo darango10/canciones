972ac0acb9e13cfd8f3d7e6cb2fc89edcf52796b
Daniel <arangomonsalve.daniel@gmail.com>
2020-05-02 16:22:14 -0500

    Busqueda de Canciones

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useEffect, useState} from 'react';
+import Formulario from "./components/Formulario";
+import axios from 'axios'
+import Cancion from "./components/Cancion";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    //Definimos state
+    const [busquedaLetra, guardarBusquedaLetra] = useState({});
+    const [letra, guardarLetra] = useState('');
+
+    useEffect(() => {
+        if (Object.keys(busquedaLetra).length === 0) return;
+
+        const consultarAPILetra = async () => {
+            const {artista, cancion} = busquedaLetra
+            const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`
+            const respuesta = await axios.get(url)
+            guardarLetra(respuesta.data.lyrics)
+        }
+        consultarAPILetra()
+    }, [busquedaLetra])
+
+    return (
+        <Fragment>
+            <Formulario guardarBusquedaLetra={guardarBusquedaLetra}/>
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col-md-6">
+                        Artista
+                    </div>
+                    <div className="col-md-6">
+                        <Cancion letra={letra}/>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    );
 }
 
 export default App;
